29858e8ff3ac9dabbad45ea867821aad37086f1e
stasithyst <pechenkinaanastasiia2003@gmail.com>
2025-12-04 17:27:30 +0300

    T.T

--- vite.config.ts
+++ vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: '/cat-projects/',
+  base: process.env.NODE_ENV === 'production' ? 'cat-projects' : '/',
 })
